611b0d59bda34eb6d771189353d2505a8ab9eeb5
Dario Civallero <dario.civallero@gmail.com>
2015-12-06 15:27:04 +0100

    Tuna incapsulated into the Effect Manager

---
+++ src/lib/core/EffectManager.js
@@ -0,0 +1,6 @@
+import AudioContext from '../AudioContext'
+import Tuna from 'tunajs'
+
+let manager = new Tuna(AudioContext);
+
+export default manager;
--- src/lib/modules/Filter.js
+++ src/lib/modules/Filter.js
@@ -1,6 +1,7 @@
 import { CONST, TYPES } from '../core/Constants'
 import AudioContext from '../AudioContext'
 import Module from '../core/Module'
+import EffectManager from '../core/EffectManager'
 
 class Filter extends Module {
 
@@ -9,39 +10,46 @@ class Filter extends Module {
 
         this.freq = +props.freq || 11000;
         this.q = +props.q || 10;
+        this.bypass = +props.bypass || 0;
+        this.filterGain = this.level || 0;
 
-        this.main = AudioContext.createBiquadFilter();
-        this.main.type = props.type || CONST.FILTER_LOWPASS;
-
-        this.setCutOff();
-        this.setQ();
+        //TODO think about a Effect super class....to cancel gain creation
+        this.main = new EffectManager.Filter({
+            frequency: this.freq, //20 to 22050
+            Q: this.q, //0.001 to 100
+            gain: this.filterGain, //-40 to 40
+            filterType: props.type || CONST.FILTER_LOWPASS,
+            bypass: this.bypass
+        });
     }
 
     createGain () {
         return false;
     }
 
-    setCutOff () {
-        let cutOff = ((this.freq - 20) / (20000 - 20)) * (14.287712379549449 - 0) + 0;
-        this.main.frequency.value = Math.pow(2, cutOff);
-    }
-
-    setQ () {
-        let q = this.q % 21;
-        this.main.Q.value = q;
-    }
-
     getLineIn (source) {
         if (source === TYPES.MODULATOR) {
             //TODO let main parameter detune | frequency... configurable
-            return this.main.detune;
+            return this.main.filter.detune;
         } else {
-            return this.main;
+            return this.main.input;
         }
     }
 
     getLineOut () {
-        return this.main;
+        return this.main.output;
+    }
+
+    getEnvelopeTarget (target) {
+        let ret = null;
+
+        if (target === 'gain') {
+            ret = this.main.output.gain;
+        } else if (this.main && this.main.filter && this.main.filter[target]) {
+            ret = this.main.filter[target]
+        }
+
+        return ret;
     }
 }
 
