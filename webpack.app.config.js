b20790aef153c64adc248741e00b0c0748d5271b
Dario Civallero <dario.civallero@gmail.com>
2016-01-19 22:11:41 +0100

    webpack config refactoring

--- webpack.app.config.js
+++ webpack.app.config.js
@@ -1,120 +1,19 @@
-var path = require('path'),
-    webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    autoprefixer = require('autoprefixer'),
-    workingDirectory = path.join(__dirname, 'app'),
-    config = {};
+(function (module) {
+    'use strict';
 
-//-------------- CONFIGURATION ------------------------
-if(process.argv && process.argv.length && process.argv.indexOf('-build') !== -1) {
-    console.log('/***** APPLICATION BUILD ****/');
-    config.watch = false;
-    config.outputFile = 'js/bundle.min.js';
-    config.plugins = [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ minimize: true }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'app/index.html'),
-            inject:   'body'
-        }),
-        new ExtractTextPlugin('screen.css')
-    ];
-    config.jshintConfig = {
-        emitErrors: true,
-        failOnHint: true
-    }
-} else {
-    console.log('/***** APPLICATION DEVELOPMENT ****/');
-    config.watch = true;
-    config.outputFile = 'js/bundle.js';
-    config.plugins = [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'app/index.html'),
-            inject:   'body'
-        }),
-        new ExtractTextPlugin('screen.css')
-    ];
-    config.jshintConfig = {
-        emitErrors: false,
-        failOnHint: false
-    }
-}
+    var getConfiguration = require('./webpack-configuration'),
+        config = {};
 
-//-------------- EXPORT -------------------------------
-module.exports = {
-    entry:   [
-        'babel-core/polyfill',
-        path.join(__dirname, 'app/src/app.js')
-    ],
-    output:  {
-        path:     path.join(__dirname, 'publish'),
-        filename: config.outputFile
-    },
-    plugins: config.plugins,
-    postcss: [
-        autoprefixer({
-            browsers: ['last 2 version']
-        })
-    ],
-    jshint: config.jshintConfig,
-    module:  {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                loader: 'jscs-loader',
-                include: workingDirectory
-            },
-            {
-                test: /\.js$/,
-                loader: 'jshint-loader',
-                include: workingDirectory
-            }
-        ],
-        loaders: [
-            {
-                test:    /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules|dist/
-            },
-            {
-                test: /bootstrap\/js\//,
-                loader: 'imports?jQuery=jquery'
-            },
-            {
-                test:   /\.(woff|woff2|ttf|eot)$/,
-                loader: 'file?name=assets/fonts/[name].[ext]'
-            },
-            {
-                test:   /\.(png|jpg|jpeg|gif|svg)$/,
-                loader: 'file?name=assets/img/[name].[ext]'
-            },
-            {
-                test:   /\.(txt)$/,
-                loader: 'file?name=[name].[ext]'
-            },
-            {
-                test:   /\.js\.map$/,
-                loader: 'file?name=js/[name].[ext]'
-            },
-            {
-                test:   /\.css\.map$/,
-                loader: 'file?name=[name].[ext]'
-            },
-            {
-                test:   /\.less$/,
-                loader: ExtractTextPlugin.extract('style', 'css!less!postcss')
-            },
-            {
-                test:   /\.css$/,
-                loader: ExtractTextPlugin.extract('style', 'css')
-            },
-            {
-                test:   /\.html$/,
-                loader: 'raw'
-            }
-        ]
-    },
-    watch:   config.watch
-};
+    if (process.argv && process.argv.length && process.argv.indexOf('-build') !== -1) {
+        //Build Configuration
+        console.log('/***** APPLICATION BUILD ****/');
+        config = getConfiguration('app', 'prod');
+    } else {
+        //Development Configuration
+        console.log('/***** APPLICATION DEVELOPMENT ****/');
+        config = getConfiguration('app', 'dev');
+    }
+
+    module.exports = config;
+
+})(module);
--- webpack.lib.config.js
+++ webpack.lib.config.js
@@ -1,66 +1,19 @@
-var path = require('path'),
-    webpack = require('webpack'),
-    workingDirectory = path.join(__dirname, 'lib'),
-    config = {};
+(function (module) {
+    'use strict';
 
-//-------------- CONFIGURATION ------------------------
-if (process.argv && process.argv.length && process.argv.indexOf('-build') !== -1) {
-    //Build Configuration
-    console.log('/***** LIBRARY BUILD ****/');
-    config.watch = false;
-    config.outputFile = 'web-synth.min.js';
-    config.plugins = [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ minimize: true })
-    ];
-    config.jshintConfig = {
-        emitErrors: true,
-        failOnHint: true
+    var getConfiguration = require('./webpack-configuration'),
+        config = {};
+
+    if (process.argv && process.argv.length && process.argv.indexOf('-build') !== -1) {
+        //Build Configuration
+        console.log('/***** LIBRARY BUILD ****/');
+        config = getConfiguration('lib', 'prod');
+    } else {
+        //Development Configuration
+        console.log('/***** LIBRARY DEVELOPMENT ****/');
+        config = getConfiguration('lib', 'dev');
     }
-} else {
-    //Development Configuration
-    console.log('/***** LIBRARY DEVELOPMENT ****/');
-    config.watch = true;
-    config.outputFile = 'web-synth.js';
-    config.plugins = [
-        new webpack.optimize.OccurenceOrderPlugin()
-    ];
-    config.jshintConfig = {
-        emitErrors: false,
-        failOnHint: false
-    }
-}
 
-//-------------- EXPORT -------------------------------
-module.exports = {
-    entry: path.join(__dirname, 'lib/index.js'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: config.outputFile
-    },
-    plugins: config.plugins,
-    jshint: config.jshintConfig,
-    module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                loader: 'jscs-loader',
-                include: workingDirectory
-            },
-            {
-                test: /\.js$/,
-                loader: 'jshint-loader',
-                include: workingDirectory
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/
+    module.exports = config;
 
-            }
-        ]
-    },
-    watch: config.watch
-};
+})(module);
