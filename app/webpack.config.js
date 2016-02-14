652aa37896852ca1563cea7761244d16d28de3a8
Dario Civallero <dario.civallero@gmail.com>
2016-02-14 16:27:26 +0100

    app

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,38 +1,56 @@
-var path = require('path');
-var webpack = require('webpack');
+(function (module) {
+    'use strict';
 
-module.exports = {
-    devtool: 'eval',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        './src/index'
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    resolve: {
-        //alias: {
-        //  'redux-devtools': path.join(__dirname, '..', '..', 'src'),
-        //  'react': path.join(__dirname, 'node_modules', 'react')
-        //}
-    },
-    resolveLoader: {
-        fallback: path.join(__dirname, 'node_modules')
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['react-hot', 'babel'],
-                exclude: /node_modules/,
-                include: path.join(__dirname, 'src')
-            }
-        ]
+    var path = require('path'),
+        webpack = require('webpack'),
+        devtoolValue;
+
+    if (process.env.NODE_ENV === 'production') {
+        //Build Configuration
+        console.log('/***** APPLICATION BUILD ****/');
+
+        devtoolValue = 'source-map';
+
+    } else {
+        //Development Configuration
+        console.log('/***** APPLICATION DEVELOPMENT ****/');
+
+        devtoolValue = 'eval';
+
     }
-};
+
+    module.exports = {
+        devtool: devtoolValue,
+        entry: [
+            'webpack-dev-server/client?http://localhost:3000',
+            'webpack/hot/only-dev-server',
+            './src/index'
+        ],
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.js',
+            publicPath: '/static/'
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        resolve: {
+            alias: {
+                'web-synth': path.join(__dirname, '..', 'dist') + '/web-synth.js'
+            }
+        },
+        resolveLoader: {
+            fallback: path.join(__dirname, 'node_modules')
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loaders: ['react-hot', 'babel'],
+                    exclude: /node_modules/,
+                    include: path.join(__dirname, 'src')
+                }
+            ]
+        }
+    };
+})(module);
