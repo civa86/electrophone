461b6a673f07e4bec64ec2a68d3317e388b82fc1
Dario Civallero <dario.civallero@gmail.com>
2016-03-13 20:01:04 +0100

    public documentation

--- publish.sh
+++ publish.sh
@@ -4,16 +4,19 @@
 echo "/***** INSTALL LIBRARY DEPENDENCIES ****/"
 npm install || { echo 'INSTALL LIBRARY DEPENDENCIES: failed' ; exit 1; }
 
+echo "/***** CLEAN DISTRIBUTION ****/"
+rm -rf app/dist
+mkdir app/dist
+
 #TODO change with dist for unit testing....
 echo "/***** LIBRARY DISTRIBUTION ****/"
 npm run lib::build || { echo 'LIBRARY DISTRIBUTION: failed' ; exit 1; }
 
+echo "/***** LIBRARY DOCUMENTATION ****/"
+npm run lib::docs::public || { echo 'LIBRARY DOCS: failed' ; exit 1; }
+
 echo "/***** ENTER APPLICATION DIRECTORY ****/"
 cd app
-
-echo "/***** CLEAN DISTRIBUTION ****/"
-rm -rf dist
-mkdir dist
 
 echo "/***** INSTALL APPLICATION DEPENDENCIES ****/"
 npm install || { echo 'INSTALL APPLICATION DEPENDENCIES: failed' ; exit 1; }
@@ -33,6 +36,7 @@ git add .
 
 echo "/***** COMMIT VERSION ****/"
 PUBLISH_TIME="`date +'%Y-%m-%d %H:%M:%S'`"
+#TODO discover repo by cmd line...
 GH_REF="github.com/civa86/web-synth.git"
 
 git commit -m "[$PUBLISH_TIME] Publish gh-pages"
