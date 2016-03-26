f31e6af9d29e2b07e02de867c9ff6569781534c4
Dario Civallero <dario.civallero@gmail.com>
2016-03-26 22:12:31 +0100

    remove handler from control panel and fix of knob initialization

--- app/src/components/ControlPanel/index.js
+++ app/src/components/ControlPanel/index.js
@@ -8,41 +8,56 @@ class ControlPanel extends Component {
         const { updateModule } = this.props;
 
         if (prop.type === 'number') {
-            return <Knob property={prop} module={module} onUpdate={updateModule} />;
+            return <Knob property={prop} module={module} onUpdate={updateModule}/>;
         } else if (prop.type === 'string') {
-            return <Select property={prop} module={module} onUpdate={updateModule} />;
+            return <Select property={prop} module={module} onUpdate={updateModule}/>;
         }
     }
 
     render () {
         const
-            { isVisible, modules } = this.props,
+            { isVisible, modules, destroyModule } = this.props,
             display = isVisible ? 'block' : 'none';
 
         return (
             <div id="control-panel" className="row" style={{ display: display }}>
                 <div className="col-xs-10 col-xs-offset-1">
                     {modules.map(module =>
-                        <div className="row" key={module.id}>
+                        <div className="row" key={module.id} style={{ marginTop: '10px' }}>
                             <div className="col-xs-12">
-                                <div style={{ border: '1px solid #333' }}>
-                                    <p className="bg-primary" style={{ padding: '10px' }}>
-                                        {module.type + ' -- ' + module.id}
-                                    </p>
-                                    <div className="row">
-                                        {
-                                            module.properties
-                                                .filter(prop => prop.name !== 'link')
-                                                .map(prop =>
-                                                    <div className="col-xs-2"
-                                                         key={module.id + prop.name}>
-                                                        {this.getControlProperty(module.id, { ...prop })}
-                                                    </div>
-                                                )
-                                        }
-                                    </div>
 
+                                <div className="row bg-primary"
+                                     style={{ border: '1px solid #333', borderBottom: 'none' }}>
+                                    <div className="col-xs-12"  style={{ padding: '10px' }}>
+                                        {module.type + ' -- ' + module.id}
+
+                                        {(() => {
+                                            if (!module.isMaster) {
+                                                return (
+                                                    <div className="glyphicon glyphicon-remove pull-right"
+                                                         style={{ cursor: 'pointer' }}
+                                                         onClick={() => destroyModule(module.id)}>
+                                                    </div>
+                                                );
+                                            }
+                                        })()}
+                                    </div>
                                 </div>
+
+                                <div className="row"
+                                     style={{ border: '1px solid #333', borderTop: 'none', paddingTop: '20px' }}>
+                                    {
+                                        module.properties
+                                            .filter(prop => prop.name !== 'link')
+                                            .map(prop =>
+                                                <div className="col-xs-2"
+                                                     key={module.id + prop.name}>
+                                                    {this.getControlProperty(module.id, { ...prop })}
+                                                </div>
+                                            )
+                                    }
+                                </div>
+
                             </div>
                         </div>
                     )}
